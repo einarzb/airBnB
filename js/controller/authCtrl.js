a4efa54fbd76165487d17b475d722d40ee05ecbf
einar <einarzb@gmail.com>
2017-08-29 17:50:22 +0300

    added login form and button

--- js/controller/authCtrl.js
+++ js/controller/authCtrl.js
@@ -1,9 +1,14 @@
 app.controller('authCtrl',['$scope','authFactory',
 
 function($scope, authFactory) {
+$scope.loginForm = false;
+$scope.checkAuth = function(){
+console.log("lets check if user exist");
+}
 
 $scope.login = function (){
   console.log("im login function in ctrlr");
+  $scope.loginForm = true;
   authFactory.login();
 }
 
